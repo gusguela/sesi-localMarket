4fda53a49c065917f6aafd31246c985ddf379eed
Kinike <86970091+gusguela@users.noreply.github.com>
2026-06-18 11:14:19 -0300

    Add files via upload

---
+++ src/login/script.js
@@ -0,0 +1,16 @@
+function showPassword(){
+    
+}
+function fecharError(){
+    
+}
+function login(){
+    var nome = $("#nome").val()
+    var senha = $("#senha").val()
+
+    if(nome && senha & nome === "admin" && senha === "12345"){
+
+    }else{
+        
+    }
+}
